692ce1b4d5112080448ee95ad1d303b3f30a4662
Jakub Kolcar <j.kolcar@raynet.cz>
2026-07-02 15:42:56 +0200

    Update fishez to 0.3.1

--- Formula/fishez.rb
+++ Formula/fishez.rb
@@ -1,7 +1,7 @@
 class Fishez < Formula
   desc "Lightning-fast terminal file manager for developers"
   homepage "https://github.com/ioma8/fishez"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   depends_on "fd"
@@ -10,24 +10,24 @@ class Fishez < Formula
   on_macos do
     on_arm do
       url "https://github.com/ioma8/fishez/releases/download/v#{version}/fishez-macos-aarch64"
-      sha256 "3be005a0e6575cf06a57a07e3c6f9ee214805dc7af0f7e2296a3fff382c30d22"
+      sha256 "999e9372a400446bff2e1a2d429edfcdc440e7d94af3c67e403590debd823c14"
     end
 
     on_intel do
       url "https://github.com/ioma8/fishez/releases/download/v#{version}/fishez-macos-x86_64"
-      sha256 "8eec2f585fb9949546188c2b204b318cd65c934268f01e137eb8f84e23b3e389"
+      sha256 "0b3ef8c9e1f4b2269cd4d439cbb1d74f89f723ea7d6bbc9614e638e41b8ca383"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ioma8/fishez/releases/download/v#{version}/fishez-linux-aarch64"
-      sha256 "914b14834cb7a65e518df745dac6e2b15d2c889004a0204885b5660c42b51971"
+      sha256 "20067e8a1c40662a0cc095f2651f27eda42aff13d7e03c0be3110c96a77b0efb"
     end
 
     on_intel do
       url "https://github.com/ioma8/fishez/releases/download/v#{version}/fishez-linux-x86_64"
-      sha256 "01661e0bb91ee5f0e73c753aec096b1002000e1b224deed1e4d58f57ec9368fc"
+      sha256 "daea11b8ac2e909c9e8ee0a61d9b5768fbb7c78cfaa3b71ba8b68ecb01335e01"
     end
   end
 
